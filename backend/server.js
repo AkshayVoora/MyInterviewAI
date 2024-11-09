b3bf35bc26d2097bb5fc7ae2b071272ea16d5c5c
Harshith Reddy <reddy17@colostate.edu>
2024-11-09 15:48:29 -0500

    Completed profile page and dashboard updates

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 // User Routes
 app.use('/api/users', userRoutes);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3000;
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
